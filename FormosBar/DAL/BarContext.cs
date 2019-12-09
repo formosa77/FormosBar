a67309fc85f06fec482d82a973f7d2eb612cdd88
formosa77 <58578513+formosa77@users.noreply.github.com>
2019-12-09 11:09:41 +0800

    Initial

---
+++ FormosBar/DAL/BarContext.cs
@@ -0,0 +1,22 @@
+﻿using FormosBar.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FormosBar.DAL
+{
+    public class BarContext : DbContext
+    {
+
+        public BarContext() : base("BarContext")
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+        
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
