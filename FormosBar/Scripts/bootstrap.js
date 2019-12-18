53ecb06ab094cce54dd64473f52a7bf173ef83b2
formosa77 <58578513+formosa77@users.noreply.github.com>
2019-12-18 09:53:21 +0800

    Handle Landing Page

--- FormosBar/Scripts/bootstrap.js
+++ FormosBar/Scripts/bootstrap.js
@@ -3,6 +3,29 @@
  * Copyright 2011-2016 Twitter, Inc.
  * Licensed under the MIT license
  */
+//Handle BacktoTop Start
+
+        //Get the button:
+        mybutton = document.getElementById("back2top");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () { scrollFunction() };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+
+//Handle BacktoTop End
 
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
