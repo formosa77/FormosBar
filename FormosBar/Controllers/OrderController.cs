48891121bfb05577a1a97e4394d3f483f92c9b5e
formosa77 <58578513+formosa77@users.noreply.github.com>
2019-12-31 03:29:41 +0800

    Handle Deleting Orders

--- FormosBar/Controllers/OrderController.cs
+++ FormosBar/Controllers/OrderController.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using FormosBar.Models;
+using FormosBar.Migrations.ContextB;
 using Microsoft.AspNet.Identity;
+using Order = FormosBar.Models.Order;
+using OrderDetail = FormosBar.Models.OrderDetail;
+using System.Data.Entity;
 
 namespace FormosBar.Controllers
 {
@@ -16,12 +23,12 @@ namespace FormosBar.Controllers
             return View();
         }
 
-        [Authorize(Roles ="Customer")]
+        [Authorize(Roles = "Customer")]
         [HttpPost]
         public ActionResult Index(Models.OrderLocation postback)
         {
             if (this.ModelState.IsValid)
-            {   
+            {
                 var currentcart = Models.ShoppingCart.CartActions.CurrentShoppingCart();
                 var userId = HttpContext.User.Identity.GetUserId();
                 var userName = HttpContext.User.Identity.GetUserName();
@@ -86,5 +93,131 @@ namespace FormosBar.Controllers
                 }
             }
         }
+
+        [Authorize(Roles = "Customer")]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            using (DAL.BarContext db = new DAL.BarContext()) { 
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+            Order order = await db.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+            }
+        }
+
+        [Authorize(Roles = "Customer")]
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            using (DAL.BarContext db = new DAL.BarContext())
+            {
+
+                Order order = await db.Orders.FindAsync(id);
+                db.Orders.Remove(order);
+                await db.SaveChangesAsync();
+                return RedirectToAction("YourOrder");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        //Create Item Page - Data Post Back
+        [HttpPost]
+        public ActionResult Add(Models.OrderDetail postback)
+        {
+            if (this.ModelState.IsValid)
+            {
+                using (DAL.BarContext db = new DAL.BarContext())
+                {
+                    //Post back data and add to Item table
+                    db.OrderDetails.Add(postback);
+
+                    //Save Change
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                ViewBag.Message = "The data entry is not correct.";
+                return View(postback);
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+        public async Task<ActionResult> Edit(int? id)
+        {
+            using (DAL.BarContext db = new DAL.BarContext())
+            {
+
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                OrderDetail orderdetail = await db.OrderDetails.FindAsync(id);
+                if (orderdetail == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(orderdetail);
+            }
+        }
+
+        // POST: Orders/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(OrderDetail orderdetail)
+        {
+            using (DAL.BarContext db = new DAL.BarContext())
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(orderdetail).State = EntityState.Modified;
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                return View(orderdetail);
+            }
+        }
+
     }
 }
